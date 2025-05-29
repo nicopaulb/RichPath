5ca9cd526a7d121c2f9b2109ab77469a3351ba1f
Nicolas Besnard <nico.besnard31@gmail.com>
2025-05-29 18:51:49 +0200

    Remove reflection

--- richpath/src/main/java/com/richpath/pathparser/PathParser.kt
+++ richpath/src/main/java/com/richpath/pathparser/PathParser.kt
@@ -1,7 +1,7 @@
 package com.richpath.pathparser
 
 import android.graphics.Path
-import android.os.Build
+import androidx.core.graphics.PathParser
 
 object PathParser {
 
@@ -9,12 +9,8 @@ object PathParser {
      * @param pathData The string representing a path, the same as "d" string in svg file.
      * @return the generated Path object.
      */
-    fun createPathFromPathData(pathData: String?): Path {
-        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            PathParserCompatApi21.createPathFromPathData(pathData) ?: Path()
-        } else {
-            PathParserCompat.createPathFromPathData(pathData)
-        }
+    fun createPathFromPathData(pathData: String): Path {
+        return PathParser.createPathFromPathData(pathData) ?: Path()
     }
 
 }
